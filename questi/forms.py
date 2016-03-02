e07ea7d82cf6b9c8af6348058ae3aee1b28d3044
phil <pdienst@gmx.net>
2016-03-02 17:30:50 +0100

    format forms.py and models.py implement vote up and vote down javascript methods

--- questi/forms.py
+++ questi/forms.py
@@ -4,19 +4,17 @@ from questi.models import Answer, Question
 
 
 class QuestionForm(forms.ModelForm):
+    title = forms.CharField(min_length=10, max_length=200, widget=forms.TextInput(attrs={'size': 60}))
+    text = forms.CharField(min_length=20, max_length=2000, widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
 
-        title = forms.CharField(min_length=10, max_length=200, widget=forms.TextInput(attrs={'size': 60}))
-        text = forms.CharField(min_length=20, max_length=2000, widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-
-        class Meta:
-            model = Question
-            fields = ['title', 'text']
+    class Meta:
+        model = Question
+        fields = ['title', 'text']
 
 
 class AnswerForm(forms.ModelForm):
+    text = forms.CharField(min_length=20, max_length=2000, widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
 
-        text = forms.CharField(min_length=20, max_length=2000, widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-
-        class Meta:
-            model = Answer
-            fields = ['text']
+    class Meta:
+        model = Answer
+        fields = ['text']
