996a9e18c6989d92a9fc802ac7fcb50865a21ec1
phil <pdienst@gmx.net>
2016-03-02 17:34:02 +0100

    change url /index to /questions

--- questi/urls.py
+++ questi/urls.py
@@ -4,7 +4,7 @@ from questi import views
 
 urlpatterns = [
     url(r'^question/create/$',views.create_question,),
-    url(r'^index/$', QuestionListView.as_view(), name="question_list"),
+    url(r'^questions/$', QuestionListView.as_view(), name="question_list"),
     url(r'^question/(?P<slug>[^/]+)/$', QuestionDetailView.as_view(), name='question_detail'),
     url(r'^question/(?P<question_id>[0-9]+)/vote_up/$', views.question_vote_up,),
     url(r'^question/(?P<question_id>[0-9]+)/vote_down/$', views.question_vote_down,),
