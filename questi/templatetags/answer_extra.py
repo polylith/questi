6d37d35ff990b0b30d77e4056209717f25d3f2aa
phil <pdienst@gmx.net>
2016-04-27 12:49:53 +0200

    fix ugliness

--- questi/templatetags/answer_extra.py
+++ questi/templatetags/answer_extra.py
@@ -15,5 +15,4 @@ def answer_is_vote_down(user, answer):
     if answer.is_vote_by_user(user) == -1:
         return "active"
     else:
-        return "###´" \
-               "" \
+        return ""
