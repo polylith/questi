4f8a63e439294e0d30db9a1ca0e012a0638168d4
phil <pdienst@gmx.net>
2016-04-27 12:48:12 +0200

    add custom templatetags

--- questi/templatetags/answer_extra.py
+++ questi/templatetags/answer_extra.py
@@ -15,4 +15,5 @@ def answer_is_vote_down(user, answer):
     if answer.is_vote_by_user(user) == -1:
         return "active"
     else:
-        return ""
+        return "###´" \
+               "" \
